816f14fd6f07c0ea234f16a177632423bc61b992
Megan Mason <megmason@mac.local>
2019-03-01 08:20:09 -0700

    2nd commit

--- monica_megan_climbing_class.py
+++ monica_megan_climbing_class.py
@@ -9,25 +9,26 @@ class climbing:
 
 
     def pitches(self):
+        pitches = self.height/170 #170 m rope length
+        return pitches
 
-        pitches = self.height/100 #170 m rope length
-
-    #def hours(self):
-        #num_hours = pitches * (grade-5)
+    def hours(self):
+        hours = self.pitches() * (self.grade-5)*2
+        return hours
 
 
 
 site1 = climbing("yosemite","granite", 3000, 5.11)
 print('location',site1.location)
 print('number of pitches',site1.pitches())
-#print(site1.hours)
+print('hours to climb', site1.hours())
 
 site2 = climbing("red river gorge","sandstone", 100, 5.14)
 print('location',site2.location)
-print(site2.pitches())
-#print(site2.hours())
+print('number of pitches',site2.pitches())
+print('hours to climb', site2.hours())
 
 site3 = climbing("black cliffs","basalt", 50, 5.6)
 print('location',site3.location)
-print(site3.pitches())
-#print(site3.hours())
+print('number of pitches',site3.pitches())
+print('hours to climb', site3.hours())
